a461ae6d7c09392b24d60bea2cacfe6119011eab
George Fonseca <george.fonseca.prestserv@petrobras.com.br>
2026-09-18 16:24:36 -0300

    feat: aula 18-09

--- aula-18-09/graph.py
+++ aula-18-09/graph.py
@@ -22,3 +22,34 @@ class Graph:
             self.adjList[sink] = set()
 
         self.adjList[sink].add(source)
+
+
+    def remove_edge(self, u, v):
+        try:
+            self.adjList[u].discard(v)
+            self.adjList[v].discard(u)
+        except KeyError:
+            print(f"Node {u} or {v} not found")
+
+
+    def degree(self, u):
+        return len(self.adjList[u])
+
+    def highest_degree(self):
+        max_degree = -1
+        max_node = None
+        for u in self.adjList:
+            if self.degree(u) > max_degree:
+                max_degree = self.degree(u)
+                max_node = u
+
+        return max_node
+
+    def is_complete(self):
+        nodes = len(self.adjList)
+        for u in self.adjList:
+            neighbors_of_u = len(self.adjList[u])
+            if nodes - 1 != neighbors_of_u:
+                return False
+
+        return True
--- aula-18-09/main.py
+++ aula-18-09/main.py
@@ -1,13 +1,38 @@
-# TODO criar grafo g3
-g3 = None
+# TODO criar grafo g3 por lista de adjacencias
+from graph import Graph
+
+g3 = Graph()
+g3.add_edge(0, 1)
+g3.add_edge(0, 2)
+g3.add_edge(0, 3)
+g3.add_edge(1, 2)
+g3.add_edge(1, 3)
+g3.add_edge(2, 3)
+
+# ou, sem orientacao a objetos:
+# g3 = {0: {1, 2, 3}, 1: {0, 2, 3}, 2: {0, 1, 3}, 3: {0, 1, 2}}
 
 # TODO implementar função que recebe um grafo, nós u e v e remove a aresta (u, v) e (v, u), se existir
+print(g3.adjList)
+g3.remove_edge(5, 1)
+print(g3.adjList)
 
 # TODO implementar função que recebe um grafo, um nó e retorna seu grau
+assert g3.degree(2) == 3
 
 # TODO implementar função que recebe um grafo e retorna o nó com maior grau
+assert g3.highest_degree() in {0, 1, 2, 3}
+
+g4 = Graph()
+g4.add_edge(0, 1)
+g4.add_edge(0, 2)
+g4.add_edge(0, 3)
+g4.add_edge(1, 3)
+assert g4.highest_degree() == 0
 
 # TODO implementar função que recebe um grafo e retorna se é completo
+assert g3.is_complete() == True
+assert g4.is_complete() == False
 
 # TODO implementar função que recebe um grafo e retorna se é regular
 
